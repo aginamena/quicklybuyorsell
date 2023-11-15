50350fce802735f152789eb7c9d1b025c654dc55
aginamena <aginamena5@gmail.com>
2023-11-14 22:31:33 -0500

    added reviews to the homepage

--- src/pages/root/reviews_and_ratings/index.jsx
+++ src/pages/root/reviews_and_ratings/index.jsx
@@ -12,12 +12,20 @@ export default function ReveiwsAndRatings() {
       </Typography>
       <Box style={{ display: "flex", justifyContent: "space-evenly" }}>
         <ReviewsAndRatingsCard
-          src="root/reviews_and_ratings/image1.jpeg"
+          src="root/reviews_and_ratings/ngozi_samuels.jpeg"
           name="Ngozi Samuels"
-          description="As a fashion seller, it's very easy to advertise my products for sale in Top fashion products. One of the reasons i like Top fashion products is that you will only find high quality products for sale here."
+          description="As a fashion seller, it's very easy to advertise my products for sale here. One of the reasons i like Top fashion products is that you will only find high quality products for sale here."
         />
-        <ReviewsAndRatingsCard src="" name="" description="" />
-        <ReviewsAndRatingsCard src="" name="" description="" />
+        <ReviewsAndRatingsCard
+          src="root/reviews_and_ratings/celeb_johnson.jpeg"
+          name="Celeb Johnson"
+          description="Connecting with sellers your local sellers is the fastest way to view and receive products, instead of waiting for days for the products to be shipped to you for review. One of the reasons i keep coming back to Top fashion products is that their site is easy to use."
+        />
+        <ReviewsAndRatingsCard
+          src="root/reviews_and_ratings/mercy_adams.jpeg"
+          name="Mercy Adams"
+          description="I sell products here and sometimes i buy from other providers. With the vast amount of products on the site, I can easily find the specific product(s) i'm looking for and contact the seller. This was how many of my clients contacted me because they could easily find my products on the site."
+        />
       </Box>
     </>
   );
