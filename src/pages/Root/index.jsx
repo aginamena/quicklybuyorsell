3a9c4d3de7cec13f2251b537f225695b0b823fa6
aginamena <aginamena5@gmail.com>
2023-11-27 21:29:44 -0500

    added value prop for buyers

--- src/pages/Root/index.jsx
+++ src/pages/Root/index.jsx
@@ -1,4 +1,4 @@
-import { Divider } from "@mui/material";
+import { Divider, Paper, Typography } from "@mui/material";
 import Footer from "components/Footer";
 import TodaysProducts from "./TodaysProducts";
 import HowItWorks from "./how_it_works";
@@ -7,8 +7,19 @@ import ReviewsAndRatings from "./reviews_and_ratings";
 export default function Root() {
   return (
     <>
-      {/* description: Discover and buy with ease from local sellers in your
-      neighborhood. image: here */}
+      <Paper
+        style={{
+          marginTop: "15px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        <Typography variant="h6">
+          Discover and buy with ease from local sellers in your neighbourhood.
+        </Typography>
+      </Paper>
       <TodaysProducts />
       <HowItWorks />
       <ReviewsAndRatings />
