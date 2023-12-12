109e42baf5a8efb7efaead57d70f60d651e441df
aginamena <aginamena5@gmail.com>
2023-12-12 11:00:29 -0500

    added pagination to published products page

--- src/pages/PublishedProducts/util.jsx
+++ src/pages/PublishedProducts/util.jsx
@@ -1,13 +1,40 @@
-import { collection, firestore, orderBy, query, where } from "config/firebase";
+import {
+  collection,
+  firestore,
+  orderBy,
+  query,
+  where,
+  limit,
+  startAfter,
+} from "config/firebase";
 import { executeQueryOnProductsCollection } from "pages/util";
 
-export async function getAllPublishedProducts(selectedCategory) {
-  const q = query(
-    collection(firestore, "products"),
-    where("category", "==", selectedCategory),
-    where("productStatus", "==", "Published"),
-    orderBy("productId", "desc")
-  );
+export async function getAllPublishedProducts(
+  selectedCategory,
+  productId,
+  setHasMore
+) {
+  const q = productId
+    ? query(
+        collection(firestore, "products"),
+        where("category", "==", selectedCategory),
+        where("productStatus", "==", "Published"),
+        orderBy("productId", "desc"),
+        startAfter(productId),
+        limit(13)
+      )
+    : query(
+        collection(firestore, "products"),
+        where("category", "==", selectedCategory),
+        where("productStatus", "==", "Published"),
+        orderBy("productId", "desc"),
+        limit(13)
+      );
+
   const products = await executeQueryOnProductsCollection(q);
-  return products;
+  if (products.length < 13) {
+    setHasMore(false);
+    return products;
+  }
+  return products.splice(0, 12);
 }
