bb742593e577d2816b1bc253f1c11fe13ff31d02
aginamena <aginamena5@gmail.com>
2023-12-25 20:11:10 -0500

    added make payment button

--- src/pages/ProductDetails/Contact.jsx
+++ src/pages/ProductDetails/Contact.jsx
@@ -1,13 +1,15 @@
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Button, Divider, Box } from "@mui/material";
 import currencyFormatter from "currency-formatter";
 import { getUser } from "pages/util";
 import { Link } from "react-router-dom";
+import { useFlutterwave, closePaymentModal } from "flutterwave-react-v3";
 
 export default function Contact({ title, amount, creatorOfProduct }) {
   const formattedAmount = currencyFormatter.format(amount, { code: "NGN" });
+  const currentUser = getUser();
+
   function handleClick() {
-    const currentUser = getUser();
     if (!currentUser) {
       alert("To contact the seller, please sign in to your account.");
       return;
@@ -19,6 +21,46 @@ export default function Contact({ title, amount, creatorOfProduct }) {
       )}`,
       "_blank"
     );
+  }
+  const config = {
+    public_key: process.env.REACT_APP_FLUTTERWAVE_PUBLIC_KEY,
+    tx_ref: Date.now(),
+    amount: amount,
+    currency: "NGN",
+    payment_options: "card",
+    customer: {
+      name: currentUser.displayName,
+      phonenumber: currentUser.phoneNumber,
+      email: currentUser.email,
+    },
+    meta: {
+      consumer_id: 23,
+      consumer_mac: "92a3-912ba-1192a",
+      // provider_details: {
+      //   ...creatorOfProduct,
+      // },
+    },
+    customizations: {
+      title: "Sending payment to QBOS",
+      description:
+        "Every transaction is charged a commission/service fee of 10%",
+    },
+  };
+  const handleFlutterPayment = useFlutterwave(config);
+
+  function handlePayment() {
+    try {
+      handleFlutterPayment({
+        callback: (response) => {
+          console.log(response);
+          closePaymentModal(); // this will close the modal programmatically
+        },
+        onClose: () => {},
+      });
+    } catch (error) {
+      console.log("an error occured");
+      console.log(error);
+    }
   }
 
   return (
@@ -69,6 +111,35 @@ export default function Contact({ title, amount, creatorOfProduct }) {
           here
         </Link>
       </small>
+      <Divider sx={{ borderBottomWidth: "5px", marginTop: "50px" }} />
+      <Button
+        variant="outlined"
+        style={{
+          width: "100%",
+          height: "60px",
+          margin: "20px 0",
+          fontSize: "25px",
+        }}
+        onClick={handlePayment}
+      >
+        Make payment
+      </Button>
+      <Divider sx={{ borderBottomWidth: "5px" }} />
+      <Box style={{ color: "#dedede", marginTop: "20px" }}>
+        <Typography style={{ textAlign: "center" }} variant="h5">
+          Safety tip
+        </Typography>
+        <Typography style={{ margin: "20px 0" }}>
+          <span style={{ marginRight: "10px" }}>1.</span>
+          Use the Make Payment button above for secure transactions; we
+          guarantee your safety.
+        </Typography>
+        <Typography>
+          <span style={{ marginRight: "10px" }}>2.</span>
+          Check the product carefully, and only make payment when you're sure
+          it's what you expected.
+        </Typography>
+      </Box>
     </Paper>
   );
 }
